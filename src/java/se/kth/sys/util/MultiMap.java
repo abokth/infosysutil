e78c1fe7428439508368f8785b9b8d63aec6140a
Rasmus Kaj <kaj@kth.se>
2009-03-16 10:44:55 +0000

    Added the MultiMap class (from MSA) to infosysutil.
    git-svn-id: https://svn-1.sys.kth.se/svn/infosys-private/repos/infosys-private/trunk/infosysutil@28244 0eaf9d90-2c94-4c3d-ac68-ce1b3f1dc49d

---
+++ src/java/se/kth/sys/util/MultiMap.java
@@ -0,0 +1,119 @@
+/*
+ * Created on 2005-maj-30
+ */
+package se.kth.sys.util;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * A Map of lists with a reasonably sane interface.
+ * @param <K> the key type
+ * @param <V> the value type
+ * @author Rasmus Kaj &lt;kaj@kth.se&gt;
+ */
+public class MultiMap<K, V> {
+
+    /** Create a new (empty) MultiMap. */
+    public MultiMap() {
+        map = new HashMap<K, List<V>>();
+    }
+
+    /** Make this map empty. */
+    public void clear() {
+        map.clear();
+    }
+
+    /**
+     * Put a value in the map.  It is possible to put multiple values
+     * with the same key in the map.
+     * @param key the key to put
+     * @param value the value add for key
+     */
+    public void put(K key, V value) {
+        List<V> list = map.get(key);
+        if (list == null) {
+            list = new ArrayList<V>();
+            map.put(key, list);
+        }
+        list.add(value);
+    }
+
+    /**
+     * Remove all values associated with a specific key from the map.
+     * @param key the key to clear.
+     */
+    public void remove(K key) {
+        map.remove(key);
+    }
+
+    /**
+     * Get the set of keys that is in this map.
+     * @return the set of keys
+     */
+    public Set<K> keySet() {
+        return map.keySet();
+    }
+
+    /**
+     * Get the set of values for a key.
+     * @param key the key to get values for
+     * @return An iterable with the values, or an empty iterable.
+     */
+    public Iterable<V> get(K key) {
+        List<V> value = map.get(key);
+        if (value != null) {
+            return value;
+        } else {
+            return new ArrayList<V>();
+        }
+    }
+
+    /**
+     * Get a single Collection containing all the values, regardless of key, in this MultiMap.
+     * @return the new summary collection
+     */
+    public Collection<V> values() {
+        Collection<V> result = new ArrayList<V>();
+        for (List<V> vlist : map.values()) {
+            for (V value : vlist) {
+                result.add(value);
+            }
+        }
+        return result;
+    }
+
+    /**
+     * Check if this MultiMap is empty or contains any values.
+     * @return true iff this MultiMap is empty.
+     */
+    public boolean isEmpty() {
+        if (map.isEmpty()) {
+            return true;            // nothing
+        }
+        for (List<V> vlist : map.values()) {
+            if (!vlist.isEmpty()) {
+                return false;       // there is values
+            }
+        }
+        return true;                // no values for any of the keys
+    }
+
+    /**
+     * Count the total number of values for all keys.
+     * @return the number of values
+     */
+    public int size() {
+        int result = 0;
+        for (List<V> vlist : map.values()) {
+            result += vlist.size();
+        }
+        return result;
+    }
+
+    private Map<K, List<V>> map;
+}
---
+++ test/java/se/kth/sys/util/MultiMapTest.java
@@ -0,0 +1,94 @@
+package se.kth.sys.util;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static se.kth.sys.util.TestUtil.assertEqualsSet;
+import static java.util.Arrays.asList;
+
+import java.util.Collections;
+
+import org.junit.Test;
+
+/**
+ * Tests for {@link MultiMap}.
+ * @author Rasmus Kaj &lt;kaj@kth.se&gt;
+ */
+public class MultiMapTest {
+
+    @Test
+    public void testDistinctValues() {
+        MultiMap<Integer, String> map = new MultiMap<Integer, String>();
+        map.put(1, "one");
+        map.put(2, "two");
+
+        assertFalse(map.isEmpty());
+        assertEquals(2, map.size());
+        assertEqualsSet("one", map.get(1));
+        assertEqualsSet("two", map.get(2));
+        assertEqualsSet(asList(1, 2), map.keySet());
+        assertEqualsSet(asList("one", "two"), map.values());
+    }
+
+    @Test
+    public void testNonDistinctValues() {
+        MultiMap<Integer, String> map = new MultiMap<Integer, String>();
+        map.put(1, "one");
+        map.put(1, "yxi");
+        map.put(2, "two");
+        map.put(2, "kaxi");
+
+        assertFalse(map.isEmpty());
+        assertEquals(4, map.size());
+        assertEqualsSet(asList("one", "yxi"), map.get(1));
+        assertEqualsSet(asList("two", "kaxi"), map.get(2));
+        assertEqualsSet(asList(1, 2), map.keySet());
+        assertEqualsSet(asList("one", "two", "yxi", "kaxi"), map.values());
+    }
+
+    @Test
+    public void testEmpty() {
+        MultiMap<Integer, String> map = new MultiMap<Integer, String>();
+
+        assertTrue(map.isEmpty());
+        assertEquals(0, map.size());
+        assertEqualsSet(Collections.<Integer>emptyList(), map.keySet());
+        assertEqualsSet(Collections.<String>emptyList(), map.values());
+    }
+
+    @Test
+    public void testClear() {
+        MultiMap<Integer, String> map = new MultiMap<Integer, String>();
+        map.put(1, "one");
+        map.put(1, "yxi");
+        map.put(2, "two");
+        map.put(2, "kaxi");
+
+        map.clear();
+
+        assertTrue(map.isEmpty());
+        assertEquals(0, map.size());
+        assertEqualsSet(Collections.<Integer>emptyList(), map.keySet());
+        assertEqualsSet(Collections.<String>emptyList(), map.values());
+    }
+
+
+    @Test
+    public void testRemoval() {
+        MultiMap<Integer, String> map = new MultiMap<Integer, String>();
+        map.put(1, "one");
+        map.remove(1);
+
+        assertTrue(map.isEmpty());
+        assertEquals(0, map.size());
+        assertEqualsSet(Collections.<Integer>emptyList(), map.keySet());
+        assertEqualsSet(Collections.<String>emptyList(), map.values());
+    }
+
+    @Test
+    public void getNonexistant() {
+        MultiMap<Integer, String> map = new MultiMap<Integer, String>();
+
+        assertEqualsSet(Collections.<String>emptyList(), map.get(3));
+    }
+}
