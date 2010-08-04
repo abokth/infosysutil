a1f3953ebdf134176f4f1920810afb512edf5309
Alexander Boström <abo@kth.se>
2010-08-04 14:42:25 +0000

    better way to ignore tests
    git-svn-id: https://svn-1.sys.kth.se/svn/infosys-private/repos/infosys-private/trunk/infosysutil@32352 0eaf9d90-2c94-4c3d-ac68-ce1b3f1dc49d

--- test/java/se/kth/sys/util/io/ReadLineThreadTest.java
+++ test/java/se/kth/sys/util/io/ReadLineThreadTest.java
@@ -13,30 +13,29 @@ import org.junit.Test;
 
 public class ReadLineThreadTest {
 
-	@Ignore("Fails to compile in hudson if uncommented") @Test
+	@Ignore("Fails to load classes in hudson") @Test
 	public void testRun() {
 		final boolean[] ok = new boolean[]{false};
 		final boolean[] fail = new boolean[]{false};
 		Reader s = new StringReader("hej");
-		fail("This test is broken somehow.");
-//		LineReceiver foo = new LineReceiver() {
-//			@Override
-//			public void receiveLine(Object bufferid, String line) {
-//				if (bufferid.equals("fooid") && line.equals("hej") && ok[0] == false)
-//					ok[0] = true;
-//				else
-//					fail[0] = true;
-//			}
-//		};
-//		final ReadLineThread r = new ReadLineThread(new BufferedReader(s), "fooid", foo);
-//		r.run();
-//		if (ok[0] == false)
-//			fail("receiveLine() was never called correctly");
-//		if (fail[0] == true)
-//			fail("receiveLine() was called incorrectly");
+		LineReceiver foo = new LineReceiver() {
+			@Override
+			public void receiveLine(Object bufferid, String line) {
+				if (bufferid.equals("fooid") && line.equals("hej") && ok[0] == false)
+					ok[0] = true;
+				else
+					fail[0] = true;
+			}
+		};
+		final ReadLineThread r = new ReadLineThread(new BufferedReader(s), "fooid", foo);
+		r.run();
+		if (ok[0] == false)
+			fail("receiveLine() was never called correctly");
+		if (fail[0] == true)
+			fail("receiveLine() was called incorrectly");
 	}
 
-	@Ignore("Fails to compile in hudson if uncommented") @Test
+	@Ignore("Fails to load classes in hudson") @Test
 	public void testGetIOException() {
 		final IOException e = new IOException("This exception should be thrown.");
 		Reader s = new StringReader("hej") {
@@ -49,16 +48,15 @@ public class ReadLineThreadTest {
 			@Override
 			public int read(CharBuffer target) throws IOException { throw e; }
 		};
-		fail("This test is broken somehow.");
-//		LineReceiver foo = new LineReceiver() {
-//			@Override
-//			public void receiveLine(Object bufferid, String line) {
-//			}
-//		};
-//		final ReadLineThread r = new ReadLineThread(new BufferedReader(s), "fooid", foo);
-//		r.run();
-//		if (e != r.getIOException())
-//			fail("The IOException was not caught and saved.");
+		LineReceiver foo = new LineReceiver() {
+			@Override
+			public void receiveLine(Object bufferid, String line) {
+			}
+		};
+		final ReadLineThread r = new ReadLineThread(new BufferedReader(s), "fooid", foo);
+		r.run();
+		if (e != r.getIOException())
+			fail("The IOException was not caught and saved.");
 	}
 
 }
